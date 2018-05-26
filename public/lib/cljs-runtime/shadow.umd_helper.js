cc2099595c237bcec72a8e3c593cd65f2cb5fd45
loganpowell <loganp@tepper.cmu.edu>
2018-05-26 13:13:08 -0400

    go blocks a go

--- public/lib/cljs-runtime/shadow.umd_helper.js
+++ public/lib/cljs-runtime/shadow.umd_helper.js
@@ -2,7 +2,7 @@ goog.provide('shadow.umd_helper');
 goog.require('cljs.core');
 goog.require('census.discovery');
 shadow.umd_helper.get_exports = (function shadow$umd_helper$get_exports(){
-var obj23351 = {"vintages":census.discovery.vintages};
-return obj23351;
+var obj23361 = {"vintages":census.discovery.vintages};
+return obj23361;
 });
 goog.exportSymbol('shadow.umd_helper.get_exports', shadow.umd_helper.get_exports);
--- src/utils/async-fetch.cljs
+++ src/utils/async-fetch.cljs
@@ -1,14 +1,18 @@
 (ns utils.async_fetch
   (:require  [ajax.core :as ajx]
-             [cljs.core.async :as a :refer [go go-loop]]))
+             [cljs.core.async :refer [>! <! chan put! take! close!]]
+             [cljs.core.async :refer-macros [go go-loop alt!]]))
+
+(go (.log js/console (<! (go 5))))
+
 
 (defn ajax-call "Accept a cljs-ajax request map, and returns a channel which will contain the response."
   [{:keys [method uri] :as opts}]
-  (let [=resp= (a/chan)]
+  (let [=resp= (chan)]
     (ajx/ajax-request (assoc opts
                         :handler (fn [[ok r :as data]]
                                    (if ok
-                                     (a/put! =resp= r)
+                                     (put! =resp= r)
                                      (prn "AJAX Error" {:error r :request opts})))))
    =resp=))
 
@@ -20,10 +24,10 @@
   (ajax-call (assoc ajax-defaults
                :method :get :uri "https://api.census.gov/data.json")))
 
-(defn test1 []
+(let [c (test-fetch)]
   (go
-    (let [response (a/<! (test-fetch))]
-      response)))
-
-
-(test1)
+    (.log js/console "Got here")
+    (.log js/console (<! c))
+    (.log js/console "We made progress"))
+  (go ; when this following go block runs, it allows the prior go to finish
+    (.log js/console "The end")))
