2ecafb2d3305f6c1e40ebb0d5e2351e3dc1c320c
loganpowell <loganp@tepper.cmu.edu>
2018-05-27 17:00:28 -0400

    move guides to https://github.com/loganpowell/cljs-guides/

--- src/utils/async_fetch.cljs
+++ src/utils/async_fetch.cljs
@@ -1,5 +1,5 @@
 (ns utils.async_fetch
-  (:require  [ajax.core :as ajx]
+  (:require  [ajax.core :as http]
              [cljs.core.async :refer [>! <! chan put! take! close!]]
              [cljs.core.async :refer-macros [go go-loop alt!]]))
 
@@ -9,7 +9,7 @@
 (defn ajax-call "Accept a cljs-ajax request map, and returns a channel which will contain the response."
   [{:keys [method uri] :as opts}]
   (let [=resp= (chan)]
-    (ajx/ajax-request (assoc opts
+    (http/ajax-request (assoc opts
                         :handler (fn [[ok r :as data]]
                                    (if ok
                                      (put! =resp= r)
@@ -17,12 +17,14 @@
    =resp=))
 
 (def ajax-defaults "Basic options for the response format"
-  {:format (ajx/json-request-format)
-   :response-format (ajx/json-response-format {:keywords? true})})
+  {:format (http/json-request-format)
+   :response-format (http/json-response-format {:keywords? true})})
+
+(def cljsdoc "https://raw.githubusercontent.com/clojure/core.async/master/src/main/clojure/cljs/core/async.cljs")
 
 (defn test-fetch []
   (ajax-call (assoc ajax-defaults
-               :method :get :uri "https://api.census.gov/data.json")))
+               :method :get :uri cljsdoc)))
 
 (let [c (test-fetch)]
   (go
